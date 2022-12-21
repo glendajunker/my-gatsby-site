57caea928f3d7494692433f0d1587b3144b721bd
glendajunker <glenda.junker@audibene.de>
2022-12-21 19:02:49 +0100

    Add image plugins

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
     return (
         <Layout pageTitle="Home Page">
             <p>I'm making this by following the Gatsby tutorial.</p>
+            <StaticImage alt="audibene logo" src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"></StaticImage>
         </Layout>
     );
 };
